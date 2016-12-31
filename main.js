5dfd7d43168f675a2768451edf9d973b7fe1e0a3
aershov <codumentary.com@gmail.com>
2016-12-31 21:17:01 +0300

    add sets cell tests

--- main.js
+++ main.js
@@ -1,3 +1,33 @@
-function add(x, y) {
-    return x + y;
+class Field {
+    constructor(width, height) {
+        this.width = width;
+        this.height = height;
+        this._cells = Field._make2DArray(this.width, this.height);
+    }
+
+    isSet(x, y) {
+        this._checkBorders(x, y);
+        return this._cells[y][x];
+    }
+
+    set(x, y) {
+        this._checkBorders(x, y);
+        this._cells[y][x] = true;
+    }
+
+    _checkBorders(x, y) {
+        console.assert(x >= 0 && x < this.width, x);
+        console.assert(y >= 0 && y < this.height, y);
+    }
+
+    static _make2DArray(width, height) {
+        let result = new Array(height);
+        for (let i = 0; i < height; i++) {
+            result[i] = new Array(width);
+            for (let j = 0; j < width; j++) {
+                result[i][j] = false;
+            }
+        }
+        return result;
+    }
 }
--- tests.js
+++ tests.js
@@ -1,5 +1,12 @@
-describe("Figure", function () {
-    it("should add", function () {
-        expect(add(3, 6)).toEqual(9);
+describe("Field", function () {
+    it("is unset by default", function () {
+        let field = new Field(20, 20);
+        expect(field.isSet(0, 0)).toBe(false);
     });
+
+    it("sets cells", function() {
+        let field = new Field(20, 20);
+        field.set(0, 0);
+        expect(field.isSet(0, 0)).toBe(true);
+    })
 });
