5ecdd0cde840e301044b0a5873135f1a47c48f71
aershov <codumentary.com@gmail.com>
2016-12-31 21:19:18 +0300

    semicolons

--- tests.js
+++ tests.js
@@ -8,13 +8,12 @@ describe("Field", function () {
         let field = new Field(20, 20);
         field.set(0, 0);
         expect(field.isSet(0, 0)).toBe(true);
-    })
+    });
 
     it("unsets cells", function() {
         let field = new Field(20, 20);
         field.set(0, 0);
         field.unset(0, 0);
         expect(field.isSet(0, 0)).toBe(false);
-    })
-
+    });
 });
